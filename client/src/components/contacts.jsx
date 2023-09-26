ad196f69981450212d02d4a0729691a5d258cc22
Whitney-Rene <whtnchambers76@gmail.com>
2023-09-26 14:55:46 -0400

    installs masonry and adds function in contacts.jsx

--- client/src/components/contacts.jsx
+++ client/src/components/contacts.jsx
@@ -1,15 +1,38 @@
 import React, { useState } from 'react';
+import Masonry from "react-responsive-masonry"
 
 function Contacts () {
 
     const [contacts, setContacts] = useState([]);
 
-    const loadContacts = () => {
+    const loadContacts = async () => {
+        try {
+            const response = await fetch('http://localhost:1965/contacts');
 
-        
+            if(!response.ok) {
+                throw new Error ("Failed to fetch contacts");
+            }
+            
+            const fetchContacts = await response.json();
+            setContacts(fetchContacts);
+        } catch (error) {
+            console.error('Error loading contacts')
+        }
+
     }
+
+    const loadStudents = () => {
+        // A function to fetch the list of students that will be load anytime that list change
+        fetch("http://localhost:8080/api/students")
+        .then((response) => response.json())
+        .then((students) => {
+            setStudents(students);
+        });
+    };
     return (
         <>
+        <Masonry>
+        </Masonry>
         </>
     )
 
